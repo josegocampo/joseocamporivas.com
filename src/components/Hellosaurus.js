44ef66efa30f9c8f086ef39bff261ee8e9eec17e
josegocampo <jose.ocampo@mail.udp.cl>
2020-09-16 15:31:54 -0300

    hellosauruss

--- src/components/Hellosaurus.js
+++ src/components/Hellosaurus.js
@@ -6,7 +6,7 @@ import { device } from './utils/MQs'
 import React, { useState } from 'react'
 import '../App.css';
 import mejor from '../images/mejorcalidad.png';
-import john from '../images/john.jpg';
+import hellosaurus from '../images/hellosaurus.png';
 
 
 
@@ -268,25 +268,29 @@ function Conway() {
 
 
 
-
+        
 
           <div className="essay">
 
             <Titulo css={{marginTop: 30}} className="titulo">Why you should hire Jose Ocampo as Hellosaurus JS Intern</Titulo>
            <Image> <img src={mejor} className="me2" /> <span css={{color: 'rgb(152 152 152)', fontSize: '0.9rem'}}><span css={{color: 'pink'}}>Jose Ocampo</span><br/>September 15, 2020  · 4 minutes reading time</span></Image>
-            <p css={{color: 'rgba(117, 117, 117, 1)', fontSize: '1.4rem', lineHeight: '2.2rem'}}>
+            <p css={{color: 'rgba(117, 117, 117, 1)', fontSize: '1.2rem', lineHeight: '2rem'}}>
   
-            I want to share a little bit about why I'm excited for the position, why I think I am a perfect fit for the role and also in the end give you guys some info which could be helpful for you, all in under 1000 words (aka 3 minutes).
+            I want to share a little bit about why I'm <strong>excited for the position</strong>, why I think I am a <strong>perfect fit for the role </strong>and also in the end <strong>give you guys some info which could be helpful</strong> for you, all in under 1000 words.
 
 </p>
-            <Img src={john}/>
+            <Img src={hellosaurus}/>
             <p>I have done my research:
-.</p>
-            <p>I have read all that I’ve been able to find about Hellosaurus, talked with a few acquaintances that also got into this YC batch, watched the birthday show and read about entertainment, education and specifically online education for kids, and I am truly excited about the opportunity and the market that Hellosaurus is solving for. By the way, the comments were very good about the Startup and about James.
+</p>
+            <p>Spent this week reading all that I’ve been able to find about Hellosaurus, 
+                <strong> talked with a few acquaintances that also got into this YC batch*</strong>, 
+                watched the <strong>birthday show</strong> , read about entertainment, 
+                education, specifically online education for kids, and I am truly excited about the opportunity and the market that Hellosaurus is solving for. 
+                *By the way, the comments were very good about the Startup and about James.
  </p>
  
             <p>
-I think this is an excellent opportunity and time to help parents and provide fun, entertainment and education to kids all over the world.
+I think this is an excellent opportunity and time to <strong>help parents</strong> and provide education and entertainment in an interactive way to kids all over the world.
 </p>
 
 
@@ -296,12 +300,21 @@ I would love to be part of the team and help Hellosaurus grow as much as I can!
 </p>
 
 <p>
-I am a Web Developer and also a Lawyer, but more importantly I like to consider myself as a Full Stack problem solver and facilitator, the tool that will be used depends on the context of the problem we are trying to solve itself. 
+I am a Web Developer and also a Lawyer, but more importantly I like to consider myself as a <strong>Full Stack problem solver and facilitator</strong>, 
+the tool that will be used depends on the context of the problem we are trying to solve itself. 
 
 </p>
 
 <p>
-At the previous Startup I worked at, a Legaltech where I was the first employee, -in which we qualified and went to the YC interview last December, but sadly didn't get in-, I wore many hats, from taking care of sales, helping with the development of the software and marketing pages, copywriting, creating content, managing social media, to reviewing and drafting contracts, and managing a team of 12 lawyers distributed all over the world. I was kind of like a mini CEO and my job was making the CEO’s life easier and solving the problems that needed taking care of. During that time we escalated from 60k annual recurring revenue to 200k, raised seed money, and moved to international markets.
+At the previous Startup I worked at, a Legaltech where I was the first employee, 
+-in which <strong>we qualified and went to the YC interview last December</strong>, but sadly didn't get in-, 
+I wore many hats. 
+
+</p><p>From taking care of <strong>sales</strong>, helping with the <strong>development of the software</strong> and marketing pages, 
+<strong> copywriting</strong>, <strong>creating content</strong>, managing social media, to r<strong>eviewing and drafting contracts</strong>, 
+and <strong>managing</strong> a team of 12 lawyers distributed all over the world. 
+I was kind of like a mini CEO and my job was making the CEO’s life easier and solving the problems that needed taking care of.
+ During that time <strong>we escalated from 60k annual recurring revenue to 200k</strong>, <strong>raised seed money</strong>, and <strong>moved to international markets</strong>.
 
 </p>
 
@@ -312,22 +325,30 @@ The tools I specifically use to solve a problem will depend on what you need me 
 </p>
 
 <p>
-I know this position if for a JavaScript internship, for which I am very well qualified, having studied Web Development and Computer Science at Lambda School (another YC company) and having built several apps using technologies based on JavaScript at its core, which I hope to have the opportunity to show to you.
+I know this position if for a <strong>JavaScript internship</strong>, 
+for which I am <strong>very well qualified</strong>, having studied <strong>Web Development and Computer Science</strong> at Lambda School (another YC company) 
+and having <strong>built several apps using technologies based on JavaScript</strong> at its core, which I hope to have the opportunity to show to you.
 
 </p>
 
-<p>My main stack is React with Redux and Styled Components or CSS w/LESS for the FrontEnd and Node.js with Express, Knex, Relational Databases and Rest APIs for the Backend.
+<p>My main stack is <strong>React</strong> with Redux and 
+    Styled Components or CSS w/LESS for the FrontEnd and <strong>Node.js</strong> with Express, Knex, Relational Databases and Rest APIs for the Backend.
 </p>
 
-<p>I love building apps and solving complex problems, while providing a clean and ordered solution, but I am always open to help with whatever is needed. I know first hand that Startups sometimes have the need for team members to do many different things.
+<p>I <strong>love building</strong> apps and solving complex problems, 
+    while providing a clean and ordered solution, but I am always open to help with whatever is needed.
+     I know first hand that Startups sometimes have the need for team members to do many different things.
 </p>
 
  <p>
- I am also the father of an amazing 2 year old and that makes me even more connected and excited for what we will be building.
+ I am also the <strong>father</strong> of an amazing 2 year old and that makes me even more connected and excited for what we will be building.
 
  </p>
 
-<p>I know you might be thinking I'm too far away, but what does physical distance even mean in these days really, everyone is working remotely and we might be for a while, and we only have 1 hour difference from Chile to NY! I also have family in the U.S, my godmother (who is my aunt) lives in Berkeley, and my cousins are split between SF, Oakland and Berkeley too, and I have a cousin living in NY who stayed there after going for a MPA in Columbia. </p>
+<p>I know you might be thinking I'm too far away, 
+    but what does physical distance even mean in these days really, everyone is working remotely and we might be for a while, 
+    and we <strong>only have 1 hour difference from Chile to NY</strong>! I also have family in the U.S, my godmother (who is my aunt) lives in Berkeley, and my 
+    cousins are split between SF, Oakland and Berkeley too, and I have a cousin living in NY who stayed there after going for a MPA in Columbia. </p>
 
 
 
@@ -336,23 +357,22 @@ I know this position if for a JavaScript internship, for which I am very well qu
 
 
 <p>
-Also Chile and the U.S have a free trade agreement which means chileans are one out of of -I think- 2 nationalities in the world (Chile and Singapore) to be applicable for a H-1B1 visa, which is a lot easier to get and has a lot of privileges for chileans to go and work there on specialized industries, so if it comes to relocating in the future that is certainly an option too. I know I'm getting ahead of myself here, but just clearing some could be doubts.
+Also Chile and the U.S have a free trade agreement which means chileans 
+are one out of of -I think- 2 nationalities in the world (Chile and Singapore) to be applicable for a H-1B1 visa, 
+which is a lot easier to get and has a lot of privileges for chileans to go and work there on specialized industries,
+ so if it comes to relocating in the future that is certainly an option too. I know I'm getting ahead of myself here, but just clearing some could be doubts.
 
 
 </p>  
 
-<p>
-  A pesar de todo esto, John Conway no le tenía mucho cariño al Juego de la Vida, porque si bien el estaba feliz de su experimento, creía que la gente le daba demasiada importancia al juego y esto opacaba otros descubrimientos y pruebas que logro que le parecían mucho más interesantes y con mas aplicaciones.
-
-</p>  
 
 
-<p><h2 css={{marginTop: 70}}>Regardless of your decision to choose me or not, I found two very interesting content creators that I think would be a perfect fit for creating on and endorsing Hellosaurus. 
-</h2></p>
+<p><h4 css={{marginTop: 40}}>Regardless of your decision to choose me or not, I found two very interesting content creators that I think would be a perfect fit for creating on and endorsing Hellosaurus. 
+</h4></p>
 
 
 
-<p>One is Ana Lorena Fabrega, this is her website. She is a former elementary school teacher who transitioned into being an advocate for online and alternative learning methods for kids and is currently associated with David Perell (maybe you have heard of him), creating very interesting content around education and learning for kids and has quite an audience.
+<p><h3>Ana Lorena Fabrega</h3>This is <a href="https://www.analorenafabrega.com/">her website</a>. She is a former elementary school teacher who transitioned into being an advocate for online and alternative learning methods for kids and is currently associated with David Perell (maybe you have heard of him), creating very interesting content around education and learning for kids and has quite an audience on twitter and buildng on youtube.
 
 </p>
 
@@ -360,7 +380,7 @@ Also Chile and the U.S have a free trade agreement which means chileans are one 
 She is super extroverted and likeable too, has an very entertaining persona, I think she could be a great fit for Hellosaurus in many different qualities. 
 </p>  
 <p>
-The other is Kenziee Wenz , probably not so well known, but went viral a few days ago when someone shared one of her TikTok videos teaching her kindergarten students online. She clearly has a huge passion for what she does, and I could instantly imagine her creating content for Hellosaurus. Here is the video https://www.tiktok.com/@kenziiewenz/video/6872410436180921605?lang=es
+<h3>Kenziee Wenz</h3>Probably not so well known, but went viral a few days ago when someone shared one of her TikTok videos teaching her kindergarten students online. She clearly has a huge passion for what she does, and I could instantly imagine her creating content for Hellosaurus. <a href="https://www.tiktok.com/@kenziiewenz/video/6872410436180921605?lang=es">Here is the video </a>
 </p>  
 
 <p>
@@ -374,9 +394,9 @@ Best Regards,
 Jose
 </p>  
 <p>
-Linkedin: https://www.linkedin.com/in/joseocampo/?locale=en_US
-Twitter: https://twitter.com/josegocampo
-Personal Website: https://www.joseocampo.org/conway   (only in Spanish for now, translated version coming)
+<a href="https://www.linkedin.com/in/joseocampo/?locale=en_US">Linkedin | </a> 
+<a href="https://twitter.com/josegocampo">Twitter | </a> 
+<a href="https://www.joseocampo.org/">Personal</a> 
 </p>  
 
 
